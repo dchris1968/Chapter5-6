dfad5271991c765acf0b0d6780d303be34e5caf6
dchris1968 <dchris@yahoo.com>
2014-01-08 12:54:04 -0500

    First Commit

---
+++ UploadImage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML5>
+<html>
+
+<head>
+	<title>Uploading Zodiac Images</title>
+	<meta charset="utf-8" />
+	<style>
+		h1{
+			text-align:center;
+		}
+		h2{
+			text-align:center;
+		}
+	</style>
+</head>
+
+<body>
+
+		<h1>Uploading Zodiac Images</h1>
+			<form method="post"	enctype="multipart/form-data">
+				<label for="file">Filename:</label>
+				<input type="file" name="image" id="file"><br>
+				<input type="submit" name="image" value="Submit">
+			</form>
+			
+	
+        
+</body>
+</html>
