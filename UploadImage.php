a32901aa8ea86217f99f78fce0344526935ab50c
dchris1968 <dchris@yahoo.com>
2014-01-10 11:52:23 -0500

    Fixed upload images
    Completed the upload php

--- UploadImage.php
+++ UploadImage.php
@@ -17,13 +17,30 @@
 <body>
 
 		<h1>Uploading Zodiac Images</h1>
-			<form method="post"	enctype="multipart/form-data">
+			<form method="post" action="<?php echo $_SERVER['SCRIPT_NAME'];?>" enctype="multipart/form-data">
 				<label for="file">Filename:</label>
 				<input type="file" name="image" id="file"><br>
-				<input type="submit" name="image" value="Submit">
+				<input type="submit" name="submit" value="Submit">
 			</form>
 			
+<?php 
+		$Dir = "images";
+		if (isset($_POST["submit"]))
+		{
+			if (isset($_FILES['image']))
+			{
+				if (move_uploaded_file($_FILES['image']['tmp_name'], $Dir ."/" . $_FILES['image']['name']) == TRUE)
+				{
+					chmod($Dir ."/". $_FILES['image']['name'], 0644);
+					echo "File \"". htmlentities($_FILES['image']['name']). "\"successfully uploaded. <br />\n";
+				}
+				else
+				{
+					echo "There was an error uploading \"" . htmlentities($_FILES['image']['name']) . "\".<br />\n";
+				}
+			}
+		}
 	
-        
+?>
 </body>
 </html>
